a9db4cd6d9d4677b3d03a1a10b70b24c6efe8dfe
Kay Cee <sekaycee@gmail.com>
2023-06-02 01:29:48 +0100

    feat: add users list feature

--- front/src/features/users/UsersList.js
+++ front/src/features/users/UsersList.js
@@ -1,7 +1,44 @@
+import { useGetUsersQuery } from "./usersApiSlice"
+import User from './User'
+
 const UsersList = () => {
-  return (
-    <h1>Users List</h1>
-  )
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetUsersQuery()
+
+  let content
+  if (isLoading) content = <p>Loading...</p>
+
+  if (isError) {
+      content = <p className="errmsg">{error?.data?.message}</p>
+  }
+
+  if (isSuccess) {
+    const { ids } = users
+    const tableContent = ids?.length
+        ? ids.map(userId => <User key={userId} userId={userId} />)
+        : null
+
+    content = (
+      <table className="">
+        <thead className="">
+          <tr>
+            <th scope="col" className="">Username</th>
+            <th scope="col" className="">Roles</th>
+            <th scope="col" className="">Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableContent}
+        </tbody>
+      </table>
+    )
+  }
+  return content
 }
 
 export default UsersList
