0d88d06ce9d7630148e22a2c877216881909774b
Kay Cee <sekaycee@gmail.com>
2023-06-05 01:50:45 +0100

    feat: apply JWT verification to user and project routes

--- back/routes/projectRoutes.js
+++ back/routes/projectRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const projectsController = require('../controllers/projectsController')
+const verifyJWT = require('../middleware/verifyJWT')
+
+router.use(verifyJWT)
 
 router.route('/')
   .get(projectsController.getAllProjects)
