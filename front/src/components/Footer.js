feb8809d29c1581ff54d747e44d3161c6aef88d0
Kay Cee <sekaycee@gmail.com>
2023-06-11 20:31:04 +0100

    refactor: center footer items on medium to small screens

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -4,13 +4,10 @@ import { Facebook, Instagram, Twitter } from 'react-bootstrap-icons'
 const linkStyle = 'block mt-3 text-sm font-medium text-gray-600 duration-300 hover:text-blue-700'
 const services = [
   { name: 'Translation', class: linkStyle, href: '/services/translation' },
-  { name: 'Digital Marketing', class: linkStyle, href: '/services/digital-marketing' },
   { name: 'Writing', class: linkStyle, href: '/services/writing' },
   { name: 'Programming', class: linkStyle, href: '/services/programming' },
   { name: 'Design', class: linkStyle, href: '/services/design' },
-  { name: 'Proofreading', class: linkStyle, href: '/services/proofreading' },
-  { name: 'Photography', class: linkStyle, href: '/services/photography' },
-  { name: 'Accommodation', class: linkStyle, href: '/services/accommodation' },
+  { name: 'Proofreading', class: linkStyle, href: '/services/proofreading' }
 ]
 const company = [
   { name: 'Leadership', class: linkStyle, href: '/company/team' },
@@ -18,9 +15,6 @@ const company = [
   { name: 'Affiliates', class: linkStyle, href: '/company/affiliates' },
   { name: 'Our Impact', class: linkStyle, href: '/company/impact' },
   { name: 'Podcast', class: linkStyle, href: '/company/podcast' },
-  { name: 'Licensing', class: linkStyle, href: '/company/licensing' },
-  { name: 'Privacy Policy', class: linkStyle, href: '/company/privacy' },
-  { name: 'Terms of Service', class: linkStyle, href: '/company/terms' },
 ]
 const about = [
   { name: 'Introduction', class: linkStyle, href: '/about/us' },
@@ -35,27 +29,18 @@ const support = [
   { name: 'FAQs', class: linkStyle, href: '/support/faqs' },
   { name: 'Inquiries', class: linkStyle, href: '/support/inquiries' },
 ]
+const legal = [
+  { name: 'Cookies', class: linkStyle, href: '/legal/cookies' },
+  { name: 'Licensing', class: linkStyle, href: '/legal/licensing' },
+  { name: 'Privacy', class: linkStyle, href: '/legal/privacy' },
+  { name: 'Terms', class: linkStyle, href: '/legal/terms' },
+]
 
 const Footer = () => {
   const content = (
     <footer className='px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
-      {/* <div class='mx-auto max-w-screen-2xl mb-12'>
-        <div class='flex flex-col items-center justify-between gap-2 border-t border-b py-7 md:flex-row'>
-          <div class='mb-3 text-center md:mb-0 md:text-left'>
-            <span class='font-bold uppercase tracking-widest text-gray-800'>Newsletter</span>
-            <p class='text-gray-500'>Subscribe to our newsletter</p>
-          </div>
-
-          <form class='flex w-full gap-2 md:max-w-md'>
-            <input placeholder='Email' class='w-full flex-1 rounded-md border bg-gray-50 px-3 py-2 text-gray-800 placeholder-gray-500 outline-none ring-blue-300 transition duration-100 focus:ring' />
-
-            <button class='inline-block rounded-md bg-blue-500 px-8 py-2 text-center text-sm font-semibold text-white outline-none ring-blue-300 transition duration-100 hover:bg-blue-600 focus-visible:ring active:bg-blue-700 md:text-base'>Send</button>
-          </form>
-        </div>
-      </div> */}
-
       <div className='grid gap-16 row-gap-10 mb-8 lg:grid-cols-6'>
-        <div className='md:max-w-md lg:col-span-2'>
+        <div className='text-center md:text-left md:max-w-md lg:col-span-2'>
           <a
             href='/'
             aria-label='Go home'
@@ -70,37 +55,57 @@ const Footer = () => {
               Consulting
             </span>
           </a>
-          <div className='mt-4 lg:max-w-sm'>
+          <div className='my-4 lg:max-w-sm'>
             <p className='text-sm text-gray-800'>
               Welcome to HubX, the consultancy arm of HubX limited: 
               Founded in March 2023 with UK company registration number 14722644.
             </p>
             <p className='mt-4 text-sm text-gray-800'>
-              At HubX, we believe that every student has the potential to succeed and excel, 
-              and it is our mission to help them realise their full potential.
+              At HubX, we believe that every student and professional has the potential to excel, 
+              as such we've made it our mission to help them in their pursuit of excellence.
             </p>
+          </div>
+          <div className='text-center lg:text-left'>
+            <a
+              href='https://twitter.com/hubx-consulting'
+              className='inline-block mr-4 text-gray-500 transition-colors duration-300 hover:text-blue-400'
+            >
+              <Twitter size={16} />
+            </a>
+            <a
+              href='https://instagram.com/hubx-consulting'
+              className='inline-block mr-4 text-gray-500 transition-colors duration-300 hover:text-purple-400'
+            >
+              <Instagram size={16} />
+            </a>
+            <a
+              href='https://facebook.com/hubx-consulting'
+              className='inline-block text-gray-500 transition-colors duration-300 hover:text-blue-700'
+            >
+              <Facebook size={16} />
+            </a>
           </div>
         </div>
         <div className='grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4'>
-          <div>
+          <div className='text-center md:text-left'>
             <div className='text-xs font-semibold text-gray-800 tracking-wide uppercase'>Category</div>
             {services.map((link) => (
               <a className={link.class} href={link.href}>{link.name}</a>
             ))}
           </div>
-          <div>
+          <div className='text-center md:text-left'>
             <div className='text-xs font-semibold text-gray-800 tracking-wide uppercase'>Company</div>
             {company.map((link) => (
               <a className={link.class} href={link.href}>{link.name}</a>
             ))}
           </div>
-          <div>
+          <div className='text-center md:text-left'>
             <div className='text-xs font-semibold text-gray-800 tracking-wide uppercase'>About</div>
             {about.map((link) => (
               <a className={link.class} href={link.href}>{link.name}</a>
             ))}
           </div>
-          <div>
+          <div className='text-center md:text-left'>
             <div className='text-xs font-semibold text-gray-800 tracking-wide uppercase'>Support</div>
             {support.map((link) => (
               <a className={link.class} href={link.href}>{link.name}</a>
@@ -108,30 +113,16 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className='flex flex-col justify-between py-5 border-t sm:flex-row'>
-        <p className='text-sm text-gray-600'>
-          © Copyright 2023 HubX Consulting. All rights reserved.
+      <div className='flex flex-col items-center justify-between text-sm text-gray-600 py-5 border-t md:flex-row'>
+        <p>
+          © Copyright 2023 HubX Inc.
         </p>
-        <div className='flex items-center mt-4 space-x-4 sm:mt-0'>
-          <a
-            href='https://twitter.com/hubx-consulting'
-            className='text-gray-500 transition-colors duration-300 hover:text-blue-400'
-          >
-            <Twitter size={16} />
-          </a>
-          <a
-            href='https://instagram.com/hubx-consulting'
-            className='text-gray-500 transition-colors duration-300 hover:text-purple-400'
-          >
-            <Instagram size={16} />
-          </a>
-          <a
-            href='https://facebook.com/hubx-consulting'
-            className='text-gray-500 transition-colors duration-300 hover:text-blue-700'
-          >
-            <Facebook size={16} />
-          </a>
-        </div>
+        <nav className='flex text-center mt-3 md:mt-0 font-normal'>
+          {legal.map((item) => (
+            <a className='mx-1 duration-300 hover:text-blue-600' href={item.href}>{item.name}</a>
+          ))}
+        </nav>
+        <p className='mt-3 md:mt-0'>All rights reserved.</p>
       </div>
     </footer>
   )
---
+++ front/src/components/pages/common/NewsletterSnip.js
@@ -0,0 +1,14 @@
+<div class='mx-auto max-w-screen-2xl mb-12'>
+  <div class='flex flex-col items-center justify-between gap-2 border-t border-b py-7 md:flex-row'>
+    <div class='mb-3 text-center md:mb-0 md:text-left'>
+      <span class='font-bold uppercase tracking-widest text-gray-800'>Newsletter</span>
+      <p class='text-gray-500'>Subscribe to our newsletter</p>
+    </div>
+
+    <form class='flex w-full gap-2 md:max-w-md'>
+      <input placeholder='Email' class='w-full flex-1 rounded-md border bg-gray-50 px-3 py-2 text-gray-800 placeholder-gray-500 outline-none ring-blue-300 transition duration-100 focus:ring' />
+
+      <button class='inline-block rounded-md bg-blue-500 px-8 py-2 text-center text-sm font-semibold text-white outline-none ring-blue-300 transition duration-100 hover:bg-blue-600 focus-visible:ring active:bg-blue-700 md:text-base'>Send</button>
+    </form>
+  </div>
+</div>
