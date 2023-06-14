e962cc60a900311dba385ba5e1bc5bc37a715004
Kay Cee <sekaycee@gmail.com>
2023-06-15 00:49:27 +0100

    fix: edit intro content

--- front/src/pages/about/intro.js
+++ front/src/pages/about/intro.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Newsletter from '../slices/newsletter'
+import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
 
 const Intro = () => {
   const links = [
@@ -14,6 +15,44 @@ const Intro = () => {
     { name: 'Hours per week', value: '70' },
     { name: 'Paid time off', value: '2 months' },
   ]
+  const features = [
+    {
+      name: 'Client-centric approach',
+      description:
+        'Our consultancy is designed to put our customers first. We believe in providing personalised services that cater to the unique needs of each client.',
+      icon: CloudArrowUpIcon,
+    },
+    {
+      name: 'Integrity and ethics',
+      description:
+        'We hold ourselves to the highest standards of integrity and ethics. We believe in transparency, honesty, and accountability in all our dealings.',
+      icon: LockClosedIcon,
+    },
+    {
+      name: 'Professionalism',
+      description:
+        'We are committed to delivering professional services of the highest quality. Our team of experienced professionals is dedicated to providing accurate and up-to-date information to our clients.',
+      icon: ArrowPathIcon,
+    },
+    {
+      name: 'Innovation',
+      description:
+        'We are constantly seeking new and innovative ways to improve our services and deliver value to our customers. We invest in technology and tools that help us gather and share information more efficiently.',
+      icon: FingerPrintIcon,
+    },
+    {
+      name: 'Collaboration',
+      description:
+        'We believe in collaboration and partnership, and we work closely with our clients to ensure that they have the support they need to succeed.',
+      icon: FingerPrintIcon,
+    },
+    {
+      name: 'Passion',
+      description:
+        'We are passionate about education and helping students achieve their dreams. We are committed to delivering services that are of the highest quality and that meet the unique needs of our clients.',
+      icon: FingerPrintIcon,
+    },
+  ]
 
   const content = (
     <main>
@@ -27,7 +66,7 @@ const Intro = () => {
               For an exciting collaboration
             </p>
             <p className='text-gray-600'>
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident.
+              Welcome to HubX, the consultancy arm of HubX limited: Founded in March 2023 with UK company registration number 14722644.  At HubX, we believe that every student has the potential to succeed and excel, and it is our mission to help them realise their full potential.
             </p>
           </div>
           <div className='mt-4'>
@@ -73,8 +112,7 @@ const Intro = () => {
             <div className='mx-auto max-w-2xl lg:mx-0'>
               <h2 className='text-4xl font-bold tracking-tight text-white sm:text-6xl'>Work with us</h2>
               <p className='mt-6 text-lg leading-8 text-gray-300'>
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                fugiat veniam occaecat fugiat aliqua.
+                Our consultancy is a team of experienced professionals who are passionate and dedicated to helping students achieve their educational and career goals. We do this by providing personalised services to students looking to study abroad, secure scholarships, find job opportunities amongst other things. We understand that every student is unique, and we provide tailored advice and support to help them achieve their goals.
               </p>
             </div>
             <div className='mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none'>
@@ -97,6 +135,36 @@ const Intro = () => {
           </div>
         </div>
       </section>
+
+      <section className='py-24 sm:py-32'>
+        <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+          <div className='mx-auto max-w-2xl lg:text-center'>
+            <h2 className='text-base font-semibold leading-7 text-indigo-600'>Our values</h2>
+            <p className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+              Our creed defines our character
+            </p>
+            <p className='mt-6 text-lg leading-8 text-gray-600'>
+              Placing our client's interest at heart, is at the core of our business. We believe that our success is directly linked to the success of our clients, and we are committed to delivering services that meet their needs and exceed their expectations. Our core values can be summarized like this...
+            </p>
+          </div>
+          <div className='mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl'>
+            <dl className='grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16'>
+              {features.map((feature) => (
+                <div key={feature.name} className='relative pl-16'>
+                  <dt className='text-base font-semibold leading-7 text-gray-900'>
+                    <div className='absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600'>
+                      <feature.icon className='h-6 w-6 text-white' aria-hidden='true' />
+                    </div>
+                    {feature.name}
+                  </dt>
+                  <dd className='mt-2 text-base leading-7 text-gray-600'>{feature.description}</dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
+      </section>
+      
       <Newsletter />
     </main>
   )
