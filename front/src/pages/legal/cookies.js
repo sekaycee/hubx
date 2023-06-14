3153fc6c6e2ee7de44f0bc98d8e7c03a5f332842
Kay Cee <sekaycee@gmail.com>
2023-06-14 18:27:49 +0100

    feat: add legal info

---
+++ front/src/pages/legal/cookies.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Newsletter from '../slices/newsletter'
+
+const Cookies = () => {
+  const content = (
+    <>
+      <section className='h-screen place-items-center'>
+        <div className='text-center text-[10rem] mt-[20rem]'>Coming Soon</div>
+      </section>
+      <Newsletter />
+    </>
+  )
+  return content
+}
+
+export default Cookies
