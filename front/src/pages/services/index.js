ed2c7196dc00cb9558cb7300d77ffa2d239b0fba
Kay Cee <sekaycee@gmail.com>
2023-06-20 08:27:35 +0100

    feat: add logic to count words for instant quote

--- front/src/pages/services/index.js
+++ front/src/pages/services/index.js
@@ -17,11 +17,39 @@ import Writing from './writing'
 
 const Service = () => {
   const content = (
-    <main>
-      <section className='h-screen place-items-center'>
-        <div className='text-center text-[10rem] mt-[20rem]'>Coming Soon</div>
+    <main className='pt-[10rem] max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8'>
+      <section className='grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center'>
+        <aside className=''>
+          <InstantQuote />
+        </aside>
+        
+        <div className='relative ml-4'>
+          <img className='w-full rounded-md' src='https://images.unsplash.com/photo-1665686377065-08ba896d16fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&h=800&q=80' alt='Image Description' />
+          <div className='absolute inset-0 -z-[1] bg-gradient-to-tr from-gray-200 via-white/0 to-white/0 w-full h-full rounded-md mt-4 -mb-4 mr-4 -ml-4 lg:mt-6 lg:-mb-6 lg:mr-6 lg:-ml-6 dark:from-slate-800 dark:via-slate-900/0 dark:to-slate-900/0'></div>
+
+          <div className='absolute bottom-0 left-0'>
+            <svg className='w-2/3 ml-auto h-auto text-white dark:text-slate-900' width='630' height='451' viewBox='0 0 630 451' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <rect x='531' y='352' width='99' height='99' fill='currentColor'/>
+              <rect x='140' y='352' width='106' height='99' fill='currentColor'/>
+              <rect x='482' y='402' width='64' height='49' fill='currentColor'/>
+              <rect x='433' y='402' width='63' height='49' fill='currentColor'/>
+              <rect x='384' y='352' width='49' height='50' fill='currentColor'/>
+              <rect x='531' y='328' width='50' height='50' fill='currentColor'/>
+              <rect x='99' y='303' width='49' height='58' fill='currentColor'/>
+              <rect x='99' y='352' width='49' height='50' fill='currentColor'/>
+              <rect x='99' y='392' width='49' height='59' fill='currentColor'/>
+              <rect x='44' y='402' width='66' height='49' fill='currentColor'/>
+              <rect x='234' y='402' width='62' height='49' fill='currentColor'/>
+              <rect x='334' y='303' width='50' height='49' fill='currentColor'/>
+              <rect x='581' width='49' height='49' fill='currentColor'/>
+              <rect x='581' width='49' height='64' fill='currentColor'/>
+              <rect x='482' y='123' width='49' height='49' fill='currentColor'/>
+              <rect x='507' y='124' width='49' height='24' fill='currentColor'/>
+              <rect x='531' y='49' width='99' height='99' fill='currentColor'/>
+            </svg>
+          </div>
+        </div>
       </section>
-      <InstantQuote />
 
       <Newsletter />
     </main>
--- front/src/pages/slices/quote.js
+++ front/src/pages/slices/quote.js
@@ -1,102 +1,387 @@
-import useScripts from '../../hooks/useScripts'
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+import mammoth from 'mammoth'
+import { version, getDocument, GlobalWorkerOptions } from 'pdfjs-dist'
 
-const extractText = (src) => {
-  var pdf = pdfjsLib.getDocument(src)
-  return pdf.promise.then(function (pdf) {
-    var totalPageCount = pdf.numPages
-    var countPromises = []
-    for (
-      var currentPage = 1;
-      currentPage <= totalPageCount;
-      currentPage++
-    ) {
-      var page = pdf.getPage(currentPage)
-      countPromises.push(
-        page.then(function (page) {
-          var textContent = page.getTextContent()
-          return textContent.then(function (text) {
-            return text.items
-              .map((s) => {return s.str})
-              .join('')
-          })
-        })
-      )
+GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${version}/pdf.worker.js`
+
+const people = [
+  {
+    id: 1,
+    name: 'Wade Cooper',
+    avatar:
+      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    id: 2,
+    name: 'Arlene Mccoy',
+    avatar:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    id: 3,
+    name: 'Devon Webb',
+    avatar:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
+  },
+  {
+    id: 4,
+    name: 'Tom Cook',
+    avatar:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    id: 5,
+    name: 'Tanya Fox',
+    avatar:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    id: 6,
+    name: 'Hellen Schmidt',
+    avatar:
+      'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    id: 7,
+    name: 'Caroline Schultz',
+    avatar:
+      'https://images.unsplash.com/photo-1568409938619-12e139227838?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    id: 8,
+    name: 'Mason Heaney',
+    avatar:
+      'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    id: 9,
+    name: 'Claudie Smitham',
+    avatar:
+      'https://images.unsplash.com/photo-1584486520270-19eca1efcce5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    id: 10,
+    name: 'Emil Schaefer',
+    avatar:
+      'https://images.unsplash.com/photo-1561505457-3bcad021f8ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+const getTextFromPDF = async (file) => {
+  const fileReader = new FileReader()
+
+  return new Promise((resolve, reject) => {
+    fileReader.onload = async () => {
+      try {
+        const typedArray = new Uint8Array(fileReader.result)
+        const pdf = await getDocument(typedArray).promise
+        let extractedText = ''
+
+        for (let i = 1; i <= pdf.numPages; i++) {
+          const page = await pdf.getPage(i)
+          const content = await page.getTextContent()
+          const textItems = content.items.map((item) => item.str) 
+          extractedText += textItems.join(' ')
+        }
+        resolve(extractedText)
+      } catch (err) {
+        reject(err)
+      }
     }
-
-    return Promise.all(countPromises).then(function (texts) {
-      return texts.join('')
-    })
+    fileReader.readAsArrayBuffer(file)
   })
 }
 
+const getTextFromDOCX = async (file) => {
+  const fileReader = new FileReader()
+
+  return new Promise((resolve, reject) => {
+    fileReader.onload = async () => {
+      try {
+        const arrayBuffer = fileReader.result
+        const result = await mammoth.extractRawText({ arrayBuffer })
+        const extractedText = result.value.trim()
+
+        resolve(extractedText)
+      } catch(err) {
+        reject(err)
+      }
+    }
+    fileReader.readAsArrayBuffer(file)
+  })
+}
+
+const getTextFromTXT = async (file) => {
+  const fileReader = new FileReader()
+
+  return new Promise((resolve, reject) => {
+    fileReader.onload = () => {
+      const extractedText = fileReader.result
+
+      resolve(extractedText)
+    }
+    fileReader.onerror = reject
+
+    fileReader.readAsText(file)
+  })
+}
 
 const InstantQuote = () => {
-  useScripts('https://mozilla.github.io/pdf.js/build/pdf.js')
+  const [wordCount, setWordCount] = useState(0)
+  const [selected, setSelected] = useState(people[3])
 
-  pdfjsLib.GlobalWorkerOptions.workerSrc =
-    'https://mozilla.github.io/pdf.js/build/pdf.worker.js'
-  
-  const countWords = () => {
-    if (window.File && window.FileReader && window.FileList && window.Blob) {
-      console.log('words')
-      const doc = document.getElementById('picker')
-      const f = doc.files[0]
-      if (!f) {
-        window.alert('Failed to load file')
-        //validate file types yet to come
-      } else if (false) {
-        window.alert(f.type + ' is not a valid text file.')
-      } else {
-        let r = new FileReader() //create file reader object
-        if (f.type == 'application/pdf') {
-          r.readAsDataURL(f)
-          r.onload = () => {
-            const file = r.result
-            extractText(file).then(
-              (text) => {
-                const res = text.replace(/\n/g, ' ').split(' ')
-                console.log(res.length)
-                const wc = 'word Count = ' + res.length
-                let info = document.getElementById('wordcount')
-                info.innerHTML = wc
-              },
-              function (reason) {
-                console.error(reason)
-              }
-            )
-            return
-          }
-        } else {
-          r.readAsText(f) //read file as text
-          //attach function to execute when loading file finishes. 
-          r.onload = (e) => {
-            const txt = e.target.result
-            const res = txt.replace(/\n/g, ' ').split(' ')
-            console.log(res.length)
-            const wc = 'word Count = ' + res.length
-            let info = document.getElementById('wordcount')
-            info.innerHTML = wc
-          }
-        }
+  const countWords = (text) => {
+    let count = 1
+    let words = text.replace(/\n|\s+|\t|\r/g, ' ')
+
+    for (let i = 0; i < words.length; i++) {
+      if (words[i-1] !== ' ' && words[i] === ' ' && words[i+1] !== ' ') {
+        count++
       }
-    } else {
-      window.alert('The File APIs are not fully supported by your browser.')
+    }
+    return count
+  }
+
+  const getWordCount = async (event) => {
+    const file = event.target.files[0]
+
+    if(file) {
+      try {
+        let text = ''
+        let words = 0
+
+        if (file.type === 'application/pdf') {
+          text = await getTextFromPDF(file)
+        } else if (file.type === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
+          text = await getTextFromDOCX(file)
+        } else if (file.type === 'text/plain') {
+          text = await getTextFromTXT(file)
+        } else {
+          throw new Error('Unsupported file format')
+        }
+
+        words = countWords(text)
+        setWordCount(words)
+      } catch(err) {
+        console.error(err)
+        setWordCount(0)
+      }
     }
   }
+
   const getInstantQuote = () => {}
 
   const content = (
-    <section>
+    <div>
+      <h1 className='block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white'>Get an instant <span className='text-blue-600'>quotation</span></h1>
+      <p className='mt-3 text-lg text-gray-800 dark:text-gray-400'>Hand-picked professionals and expertly crafted solutions, designed for any kind of student or professional.</p>
+      
+      <form>
+        <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
+          <Listbox value={selected} onChange={setSelected} className='sm:col-span-3'>
+            {({ open }) => (
+              <>
+                <Listbox.Label className='block text-sm font-medium leading-6 text-gray-900'>Project category</Listbox.Label>
+                <div className='block relative mt-2'>
+                  <Listbox.Button className='block relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6'>
+                    <span className='flex items-center'>
+                      <img src={selected.avatar} alt='' className='h-5 w-5 flex-shrink-0 rounded-full' />
+                      <span className='ml-3 block truncate'>{selected.name}</span>
+                    </span>
+                    <span className='pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2'>
+                      <ChevronUpDownIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
+                    </span>
+                  </Listbox.Button>
+
+                  <Transition
+                    show={open}
+                    as={Fragment}
+                    leave='transition ease-in duration-100'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
+                  >
+                    <Listbox.Options className='absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+                      {people.map((person) => (
+                        <Listbox.Option
+                          key={person.id}
+                          className={({ active }) =>
+                            classNames(
+                              active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                              'relative cursor-default select-none py-2 pl-3 pr-9'
+                            )
+                          }
+                          value={person}
+                        >
+                          {({ selected, active }) => (
+                            <>
+                              <div className='flex items-center'>
+                                <img src={person.avatar} alt='' className='h-5 w-5 flex-shrink-0 rounded-full' />
+                                <span
+                                  className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
+                                >
+                                  {person.name}
+                                </span>
+                              </div>
+
+                              {selected ? (
+                                <span
+                                  className={classNames(
+                                    active ? 'text-white' : 'text-indigo-600',
+                                    'absolute inset-y-0 right-0 flex items-center pr-4'
+                                  )}
+                                >
+                                  <CheckIcon className='h-5 w-5' aria-hidden='true' />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </>
+            )}
+          </Listbox>
+
+          <Listbox value={selected} onChange={setSelected} className='sm:col-span-3'>
+            {({ open }) => (
+              <>
+                <Listbox.Label className='block text-sm font-medium leading-6 text-gray-900'>Assigned to</Listbox.Label>
+                <div className='block relative mt-2'>
+                  <Listbox.Button className='relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6'>
+                    <span className='flex items-center'>
+                      <img src={selected.avatar} alt='' className='h-5 w-5 flex-shrink-0 rounded-full' />
+                      <span className='ml-3 block truncate'>{selected.name}</span>
+                    </span>
+                    <span className='pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2'>
+                      <ChevronUpDownIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
+                    </span>
+                  </Listbox.Button>
+
+                  <Transition
+                    show={open}
+                    as={Fragment}
+                    leave='transition ease-in duration-100'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
+                  >
+                    <Listbox.Options className='absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'>
+                      {people.map((person) => (
+                        <Listbox.Option
+                          key={person.id}
+                          className={({ active }) =>
+                            classNames(
+                              active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                              'relative cursor-default select-none py-2 pl-3 pr-9'
+                            )
+                          }
+                          value={person}
+                        >
+                          {({ selected, active }) => (
+                            <>
+                              <div className='flex items-center'>
+                                <img src={person.avatar} alt='' className='h-5 w-5 flex-shrink-0 rounded-full' />
+                                <span
+                                  className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
+                                >
+                                  {person.name}
+                                </span>
+                              </div>
+
+                              {selected ? (
+                                <span
+                                  className={classNames(
+                                    active ? 'text-white' : 'text-indigo-600',
+                                    'absolute inset-y-0 right-0 flex items-center pr-4'
+                                  )}
+                                >
+                                  <CheckIcon className='h-5 w-5' aria-hidden='true' />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </>
+            )}
+          </Listbox>
+
+          <div className="sm:col-span-3">
+            <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+              First name
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="sm:col-span-3">
+            <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+              Last name
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class='mb-4'>
+          <label for='hs-hero-password-2' class='block text-sm font-medium dark:text-white'><span class='sr-only'>Password</span></label>
+          <input type='email' id='hs-hero-password-2' class='py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 sm:p-4 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400' placeholder='Password' />
+        </div>
+
+        <div class='grid'>
+          <button type='submit' class='py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800 sm:p-4'>Sign up</button>
+        </div>
+      </form>
+
       <div>
-        <input id='picker' accept='.txt,.pdf,.doc,.docx' type='file' onChange={countWords} />
-        <div></div>
+        <div>
+          <input id='picker' accept='.txt,.pdf,.doc,.docx' type='file' onChange={getWordCount} />
+        </div>
         <button onClick={getInstantQuote}>Get instant quote</button>
       </div>
 
       <div>
-        <p id='wordcount'></p>
+        <div id='wordcount'>
+          {wordCount && <p>{wordCount}</p>}
+        </div>
       </div>
-    </section>
+
+
+      <div className='mt-7 grid gap-3 w-full sm:inline-flex'>
+        <a className='inline-flex justify-center items-center gap-x-3 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-sm lg:text-base text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:focus:ring-offset-gray-800' href='#'>
+          Get started
+          <svg className='w-2.5 h-2.5' width='16' height='16' viewBox='0 0 16 16' fill='none'>
+            <path d='M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14' stroke='currentColor' stroke-width='2' stroke-linecap='round'/>
+          </svg>
+        </a>
+        <a className='inline-flex justify-center items-center gap-x-3.5 text-sm lg:text-base text-center border hover:border-gray-300 shadow-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:border-gray-800 dark:hover:border-gray-600 dark:shadow-slate-700/[.7] dark:text-white dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800' href='#'>
+          Contact sales team
+        </a>
+      </div>
+    </div>
   )
   return content
 }
